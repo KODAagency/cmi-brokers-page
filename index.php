dbf79ac359781fd479c7eb3e0fd89f5c5e9f9817
Ryan Atkins <ryan@koda.agency>
2015-07-09 13:38:14 -0700

    Updates

--- index.php
+++ index.php
@@ -1,57 +1,108 @@
 <!doctype html>
 <html class="no-js" lang="">
     <head>
-        <meta charset="utf-8">
+       <meta charset="utf-8">
 
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+       <title></title>
+       <meta name="description" content="">
+       <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <!-- Place favicon.ico in the root directory -->
+       <link rel="apple-touch-icon" href="apple-touch-icon.png">
+       <!-- Place favicon.ico in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+       <link rel="stylesheet" href="css/normalize.css">
+       <link rel="stylesheet" href="css/main.css">
+       <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
     <body>
 
-	<header>
-		<h1>CMI Broker's Page</h1>
-	</header>
+      <header>
+        <h1>CMI Broker's Page</h1>
+      </header>
 
-	<section class="spot-prices">
-		<dl class="spot-price-list">
-			<dt>Gold</dt>
-				<dd><span id="spot-gold"></span></dd>
-			<dt>Silver</dt>
-				<dd><span id="spot-silver"></span></dd>
-			<dt>Platinum</dt>
-				<dd><span id="spot-platinum"></span></dd>
-			<dt>Palladium</dt>
-				<dd><span id="spot-palladium"></span></dd>
-		</dl>
-	</section>
+      <section class="spot-prices">
+	<dl class="spot-price-list">
+	  <dt>Gold</dt>
+            <dd><span id="spot-gold"></span></dd>
+	  <dt>Silver</dt>
+            <dd><span id="spot-silver"></span></dd>
+	  <dt>Platinum</dt>
+            <dd><span id="spot-platinum"></span></dd>
+	  <dt>Palladium</dt>
+            <dd><span id="spot-palladium"></span></dd>
+	</dl>
+      </section>
 
-	<section>
-		<h2>Prices</h2>
+      <section>
+        <h2>Prices</h2>
 
-		<table id="prices-table" data-family="Gold Bullion Bars">
-			<thead>
-				<tr>
-					<td>Product</td>
-				</tr>
-			</thead>
-			<tbody>
-			</tbody>
-			<tfoot>
-			</tfoot>
-		</table>
-	</section>
+        <h3>Fractional Gold</h3>
+	<table class="prices-table" data-family="Fractional Gold">
+          <thead>
+            <tr>
+              <td>Product</td>
+              <td>Ounces</td>
+            </tr>
+          </thead>
+          <tbody></tbody>
+	</table>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="js/vendor/underscore-min.js"></script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
+        <h3>Gold Bullion Bars</h3>
+        <table class="prices-table" data-family="Gold Bullion Bars">
+          <thead>
+            <tr>
+              <td>Product</td>
+              <td>Ounces</td>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+        
+        <h3>Gold Bullion Bars</h3>
+        <table class="prices-table" data-family="Gold Bullion Coins">
+          <thead>
+            <tr>
+              <td>Product</td>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+
+        <h3>Gold Collectables</h3>
+        <table class="prices-table" data-family="Gold Collectables">
+          <thead>
+            <tr>
+              <td>Product</td>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+
+        <h3>Gold Numismatic</h3>
+        <table class="prices-table" data-family="Gold Numismatic">
+          <thead>
+            <tr>
+              <td>Product</td>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+
+        <h3>Gold Other Products</h3>
+        <table class="prices-table" data-family="Gold Other Products">
+          <thead>
+            <tr>
+              <td>Product</td>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+
+      </section>
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+      <script src="js/vendor/underscore-min.js"></script>
+      <script src="js/plugins.js"></script>
+      <script src="js/main.js"></script>
     </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,40 +1,42 @@
 // Spot prices
 (function () {
-	// Gold
-	var t = document.getElementsByTagName('script')[0];
-	var s = document.createElement('script'); s.async = true;
-	s.src = '//integration.nfusionsolutions.biz/client/cmi/widget/module/singlemetal/spot-gold?metal=gold';
-	t.parentNode.insertBefore(s, t);
+  // Gold
+  var t = document.getElementsByTagName('script')[0];
+  var s = document.createElement('script'); s.async = true;
+  s.src = '//integration.nfusionsolutions.biz/client/cmi/widget/module/singlemetal/spot-gold?metal=gold';
+  t.parentNode.insertBefore(s, t);
 
-	// Silver
-	var t = document.getElementsByTagName('script')[0];
-	var s = document.createElement('script'); s.async = true;
-	s.src = '//integration.nfusionsolutions.biz/client/cmi/widget/module/singlemetal/spot-silver?metal=silver';
-	t.parentNode.insertBefore(s, t);
+  // Silver
+  var t = document.getElementsByTagName('script')[0];
+  var s = document.createElement('script'); s.async = true;
+  s.src = '//integration.nfusionsolutions.biz/client/cmi/widget/module/singlemetal/spot-silver?metal=silver';
+  t.parentNode.insertBefore(s, t);
 
-	// Platinum
-	var t = document.getElementsByTagName('script')[0];
-	var s = document.createElement('script'); s.async = true;
-	s.src = '//integration.nfusionsolutions.biz/client/cmi/widget/module/singlemetal/spot-platinum?metal=platinum';
-	t.parentNode.insertBefore(s, t);
+  // Platinum
+  var t = document.getElementsByTagName('script')[0];
+  var s = document.createElement('script'); s.async = true;
+  s.src = '//integration.nfusionsolutions.biz/client/cmi/widget/module/singlemetal/spot-platinum?metal=platinum';
+  t.parentNode.insertBefore(s, t);
 
-	// Palladium
-	var t = document.getElementsByTagName('script')[0];
-	var s = document.createElement('script'); s.async = true;
-	s.src = '//integration.nfusionsolutions.biz/client/cmi/widget/module/singlemetal/spot-palladium?metal=palladium';
-	t.parentNode.insertBefore(s, t);
+  // Palladium
+  var t = document.getElementsByTagName('script')[0];
+  var s = document.createElement('script'); s.async = true;
+  s.src = '//integration.nfusionsolutions.biz/client/cmi/widget/module/singlemetal/spot-palladium?metal=palladium';
+  t.parentNode.insertBefore(s, t);
 })();
 
 $(function () {
-	var $priceTable = $('#prices-table');
+  var $priceTable = $('.prices-table');
 
-	$.getJSON('http://kodawork.com/cmi-brokers-page/proxy.php', function ($resp) {
+  $.getJSON('../proxy.php', function ($resp) {
+    $.each($priceTable, function (key, table) {
+      var family = $(table).data('family'),
+          products = _.where($resp, {ProductFamily: family});
 
-		console.log(_.where($resp, {ProductFamily: 'Fractional Gold'}));
-
-	//	$.each($resp, function (key, val) {
-	//		console.log(key, val);
-	//		$priceTable.find('tbody').append('<tr><td>' + val.Name + '</td></tr>');
-	//	});
-	});
+      $.each(products, function (key, product) {
+        console.log(product);
+        $(table).find('tbody').append('<tr><td>' + product.Name + '</td></tr>');
+      });
+    });
+  });
 });
